bc9789a9a46e60f2e50122e9c185a9f30f03ed3f
Kartikey <kartikeypathak08@gmail.com>
2025-10-29 23:18:54 +0530

    Completed the card part

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,16 +1,124 @@
-function Hero(){
-    return(
+function Hero() {
+    return (
         <section className=" flex items-center justify-center mt-15 flex-col bg-[#F2F2F6]">
             <h1 className=" text-4xl text-black font-bold">Domestic Packages</h1>
 
-            <div className=" w-full flex items-center justify-center flex-row gap-5 mt-15 ">
-                <div className=" relative bg-black h-[30rem] w-[32rem] border-b-4 border-[#F2F2F6] overflow-hidden rounded-t-4xl flex">
-                    
-                    <div className=" bg-white  rounded-t-3xl absolute bottom-0 z-10 h-[10rem] w-full ">
+            <div className=" w-[90%] flex items-center justify-start border-2 border-green-400 overflow-x-auto snap-x  hide-scrollbar space-x-10 pl-3 pr-3  mt-15 ">
+                <div className=" relative bg-black flex-shrink-0 h-[32rem] w-[22rem]  md:w-[28rem] border-[#F2F2F6] overflow-hidden rounded-3xl flex">
+                    <img src="/goa.jpg" className=" h-full w-full object-cover object-top object-center" alt="" />
+                    <div className=" bg-white  rounded-3xl absolute flex items-start justify-start flex-col   bottom-0 z-10 h-[13rem]  w-full ">
+                        <h1 className=" font-bold text-2xl text-black m-5">Goa Tour Package</h1>
+                        <h2 className=" font-semibold text-xl text-gray-500 ml-5">Goa</h2>
+                        <div className=" flex flex-row  w-full items-center">
+                            <h2 className=" font-bold text-xl text-gray-900 ml-5">Included</h2>
+                            <div className=" flex flex-row gap-4 items-center justify-center">
+                                <i className=" font-bold text-2xl text-gray-500 fa-solid fa-utensils ml-10"></i>
+                                <i class="fa-solid fa-taxi  font-bold text-2xl text-gray-500"></i>
+                                <i class="fa-solid fa-hotel font-bold text-2xl text-gray-500" ></i>
+                            </div>
+                        </div>
+                           <div className=" flex flex-row items-center gap-4 justify-center  mt-3 w-full">
+                                <div className=" w-[40%] h-12 bg-black cursor-pointer hover:bg-black/80 transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-white">Enquiry Now</h1>
+                                </div>
+                                <div className=" w-[40%] h-12 bg-[#adb5bd] cursor-pointer hover:bg-[#ced4da] transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-black">View Details</h1>
+                                </div>
+                            </div>
                     </div>
-
                 </div>
-            
+ 
+  <div className=" relative bg-black h-[32rem] flex-shrink-0 w-[22rem]  md:w-[28rem] border-[#F2F2F6] overflow-hidden rounded-3xl flex">
+                    <img src="/goa.jpg" className=" h-full w-full object-cover object-top object-center" alt="" />
+                    <div className=" bg-white  rounded-3xl absolute flex items-start justify-start flex-col   bottom-0 z-10 h-[13rem]  w-full ">
+                        <h1 className=" font-bold text-2xl text-black m-5">Goa Tour Package</h1>
+                        <h2 className=" font-semibold text-xl text-gray-500 ml-5">Goa</h2>
+                        <div className=" flex flex-row  w-full items-center">
+                            <h2 className=" font-bold text-xl text-gray-900 ml-5">Included</h2>
+                            <div className=" flex flex-row gap-4 items-center justify-center">
+                                <i className=" font-bold text-2xl text-gray-500 fa-solid fa-utensils ml-10"></i>
+                                <i class="fa-solid fa-taxi  font-bold text-2xl text-gray-500"></i>
+                                <i class="fa-solid fa-hotel font-bold text-2xl text-gray-500" ></i>
+                            </div>
+                        </div>
+                           <div className=" flex flex-row items-center gap-4 justify-center  mt-3 w-full">
+                                <div className=" w-[40%] h-12 bg-black cursor-pointer hover:bg-black/80 transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-white">Enquiry Now</h1>
+                                </div>
+                                <div className=" w-[40%] h-12 bg-[#adb5bd] cursor-pointer hover:bg-[#ced4da] transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-black">View Details</h1>
+                                </div>
+                            </div>
+                    </div>
+                </div>  <div className=" relative bg-black flex-shrink-0 h-[32rem] w-[22rem]  md:w-[28rem] border-[#F2F2F6] overflow-hidden rounded-3xl flex">
+                    <img src="/goa.jpg" className=" h-full w-full object-cover object-top object-center" alt="" />
+                    <div className=" bg-white  rounded-3xl absolute flex items-start justify-start flex-col   bottom-0 z-10 h-[13rem]  w-full ">
+                        <h1 className=" font-bold text-2xl text-black m-5">Goa Tour Package</h1>
+                        <h2 className=" font-semibold text-xl text-gray-500 ml-5">Goa</h2>
+                        <div className=" flex flex-row  w-full items-center">
+                            <h2 className=" font-bold text-xl text-gray-900 ml-5">Included</h2>
+                            <div className=" flex flex-row gap-4 items-center justify-center">
+                                <i className=" font-bold text-2xl text-gray-500 fa-solid fa-utensils ml-10"></i>
+                                <i class="fa-solid fa-taxi  font-bold text-2xl text-gray-500"></i>
+                                <i class="fa-solid fa-hotel font-bold text-2xl text-gray-500" ></i>
+                            </div>
+                        </div>
+                           <div className=" flex flex-row items-center gap-4 justify-center  mt-3 w-full">
+                                <div className=" w-[40%] h-12 bg-black cursor-pointer hover:bg-black/80 transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-white">Enquiry Now</h1>
+                                </div>
+                                <div className=" w-[40%] h-12 bg-[#adb5bd] cursor-pointer hover:bg-[#ced4da] transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-black">View Details</h1>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                 <div className=" relative bg-black h-[32rem] flex-shrink-0 w-[22rem]  md:w-[28rem] border-[#F2F2F6] overflow-hidden rounded-3xl flex">
+                    <img src="/goa.jpg" className=" h-full w-full object-cover object-top object-center" alt="" />
+                    <div className=" bg-white  rounded-3xl absolute flex items-start justify-start flex-col   bottom-0 z-10 h-[13rem]  w-full ">
+                        <h1 className=" font-bold text-2xl text-black m-5">Goa Tour Package</h1>
+                        <h2 className=" font-semibold text-xl text-gray-500 ml-5">Goa</h2>
+                        <div className=" flex flex-row  w-full items-center">
+                            <h2 className=" font-bold text-xl text-gray-900 ml-5">Included</h2>
+                            <div className=" flex flex-row gap-4 items-center justify-center">
+                                <i className=" font-bold text-2xl text-gray-500 fa-solid fa-utensils ml-10"></i>
+                                <i class="fa-solid fa-taxi  font-bold text-2xl text-gray-500"></i>
+                                <i class="fa-solid fa-hotel font-bold text-2xl text-gray-500" ></i>
+                            </div>
+                        </div>
+                           <div className=" flex flex-row items-center gap-4 justify-center  mt-3 w-full">
+                                <div className=" w-[40%] h-12 bg-black cursor-pointer hover:bg-black/80 transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-white">Enquiry Now</h1>
+                                </div>
+                                <div className=" w-[40%] h-12 bg-[#adb5bd] cursor-pointer hover:bg-[#ced4da] transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-black">View Details</h1>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                 <div className=" relative bg-black h-[32rem] flex-shrink-0 w-[22rem]  md:w-[28rem] border-[#F2F2F6] overflow-hidden rounded-3xl flex">
+                    <img src="/goa.jpg" className=" h-full w-full object-cover object-top object-center" alt="" />
+                    <div className=" bg-white  rounded-3xl absolute flex items-start justify-start flex-col   bottom-0 z-10 h-[13rem]  w-full ">
+                        <h1 className=" font-bold text-2xl text-black m-5">Goa Tour Package</h1>
+                        <h2 className=" font-semibold text-xl text-gray-500 ml-5">Goa</h2>
+                        <div className=" flex flex-row  w-full items-center">
+                            <h2 className=" font-bold text-xl text-gray-900 ml-5">Included</h2>
+                            <div className=" flex flex-row gap-4 items-center justify-center">
+                                <i className=" font-bold text-2xl text-gray-500 fa-solid fa-utensils ml-10"></i>
+                                <i class="fa-solid fa-taxi  font-bold text-2xl text-gray-500"></i>
+                                <i class="fa-solid fa-hotel font-bold text-2xl text-gray-500" ></i>
+                            </div>
+                        </div>
+                           <div className=" flex flex-row items-center gap-4 justify-center  mt-3 w-full">
+                                <div className=" w-[40%] h-12 bg-black cursor-pointer hover:bg-black/80 transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-white">Enquiry Now</h1>
+                                </div>
+                                <div className=" w-[40%] h-12 bg-[#adb5bd] cursor-pointer hover:bg-[#ced4da] transition-all rounded-xl flex items-center justify-center">
+                                    <h1 className=" font-semibold text-[1rem] md:text-xl text-black">View Details</h1>
+                                </div>
+                            </div>
+                    </div>
+                </div>
 
             </div>
             <div className=" h-20">
